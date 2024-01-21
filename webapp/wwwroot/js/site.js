4184d0fc742ce219ace29f6e8f1afe6bcf9ffdec
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2024-01-21 15:54:46 +0800

    htmx request logging

--- webapp/wwwroot/js/site.js
+++ webapp/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.body.addEventListener("htmx:beforeRequest", (evt) => {
+    console.log(evt.detail.requestConfig)
+})
