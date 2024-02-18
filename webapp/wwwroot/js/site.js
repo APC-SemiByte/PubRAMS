f7ff563cb8dee121ed7b06207e4a070d6e7b9d9e
Aliyah Kirstie Lopez <aclopez@student.apc.edu.ph>
2024-02-19 03:58:49 +0800

    integ: add group

--- webapp/wwwroot/js/site.js
+++ webapp/wwwroot/js/site.js
@@ -5,3 +5,13 @@
 document.body.addEventListener("htmx:beforeRequest", (evt) => {
     console.log(evt.detail.requestConfig)
 })
+
+$('.modal').on('hidden.bs.modal', function (e) {
+    $(this)
+      .find("input,textarea,select")
+         .val('')
+         .end()
+      .find("input[type=checkbox], input[type=radio]")
+         .prop("checked", "")
+         .end();
+  })
